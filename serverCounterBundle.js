5bae3e85410e82fa697c16940547c8a0e3cc2d0d
im1 <im1@woowahan.com>
2020-12-09 18:20:17 +0900

    refactor: update serverCounter example files

---
+++ serverCounterBundle.js
@@ -0,0 +1,40 @@
+const Bundler = require("parcel-bundler");
+const app = require("express")();
+
+const file = "./serverCounter.html";
+const options = { watch: true };
+const { createCounter } = require("./src/backup/counter");
+
+const bundler = new Bundler(file, options);
+
+const counter = createCounter({
+  initVal: 10,
+  min: 8,
+  max: 12,
+});
+
+function response() {
+  return {
+    value: counter.val(),
+    isMax: counter.isMax(),
+    isMin: counter.isMin(),
+  };
+}
+
+app.get("/counter", (req, res) => {
+  res.send(response());
+});
+
+app.put("/counter/inc", (req, res) => {
+  counter.inc();
+  res.send(response());
+});
+
+app.put("/counter/dec", (req, res) => {
+  counter.dec();
+  res.send(response());
+});
+
+app.use(bundler.middleware());
+
+app.listen(1234);
